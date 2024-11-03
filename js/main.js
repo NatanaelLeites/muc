21d77f5764279d27bbda2da283705fa944981e2b
natanael <nathaxx09@hotmail.com>
2024-11-02 23:56:19 -0300

    modificaciones en hero, programa, footer y nombre principal

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const text = document.getElementById('myText');
 
 gsap.to(text, {
     duration: 2,
-    text: "Metodo uruguaio de cultivo",
+    text: "Metodo Uruguaio de Plantio",
     ease: "none",
 });
 gsap.from(".header", {
@@ -13,3 +13,36 @@ gsap.from(".header", {
     opacity: 0,
     duration: 1,
 });
+
+/* modal del programa */
+const modal = document.getElementById("modal");
+const btn = document.getElementById("openModal");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick   
+ = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";   
+
+  }
+}
+/* animacion modal programa */
+btn.onclick = function() {
+    modal.style.display = "block";
+    modal.classList.add('show');
+  }
+  
+  span.onclick = function() {
+    modal.classList.remove('show');
+    setTimeout(function() {
+      modal.style.display = "none";
+    }, 300);
+  }
