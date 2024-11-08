bb049233fe86fa092023ca2a2d70f1351e81b2d1
natanael <nathaxx09@hotmail.com>
2024-11-07 23:59:06 -0300

    modificacion de entradas animadas y section equipo

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ gsap.from(".header", {
     opacity: 0,
     duration: 1,
 });
+/* ScrollReveal */
+ScrollReveal().reveal('.header', {
+    delay: 400,
+    origin: 'left',
+    duration: 2000,
+    distance: '50%'
+});
+
+ScrollReveal().reveal('.curso-programa-container', {
+    delay: 200,
+    origin: 'left',
+    duration: 2000,
+    distance: '50%'
+});
+
+ScrollReveal().reveal('.equipo-container', {
+    delay: 200,
+    origin: 'right',
+    duration: 2000,
+    distance: '50%'
+   });
